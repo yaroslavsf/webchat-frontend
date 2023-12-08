b8f49e30249f9602583bc9c9149f901bce6c3c36
yaroslavsf <yaroslav1598753@gmail.com>
2023-12-08 13:01:22 +0100

    add 1 file generation

--- src/pages/build/main/api-sevice.js
+++ src/pages/build/main/api-sevice.js
@@ -1,20 +1,36 @@
-import axios from "axios";
+import axios, {create} from "axios";
+import {createFiles} from "@/pages/build/main/file-service";
 
 /**
  *
  * @param message - {actor: string, content: string}
  * @returns {Promise<void>}
  */
+function extractStringBetweenSequences( str, startStr ,endStr) {
+    const pos = str.indexOf(startStr) + startStr.length;
+    return str.substring(pos, str.indexOf(endStr, pos));
+}
 export async function request(message) {
 
-
-    axios.post(`${process.env.REACT_APP_AI_URL}/generate`, {
+    const params = "you are not allowed to write plain text with explanation and markdown for code," +
+        "the answer should be in following format: " +
+        "file: write here name of the file; " +
+        "content: write here content of the file."
+    axios.post(`${process.env.NEXT_PUBLIC_SOMETHING}/generate`, {
         model: "codellama",
-        prompt: message.content,
+        prompt: params + message.content,
         stream: false
     })
         .then(function (response) {
-            console.log(response);
+            console.log(response.data.response);
+            const file = extractStringBetweenSequences(response.data.response, "file:", "content:");
+            const content = response.data.response.split('content:').pop();
+            console.log(file)
+            console.log(content)
+            createFiles([{
+                name: file,
+                content: content
+            }])
         })
         .catch(function (error) {
             console.log(error);
