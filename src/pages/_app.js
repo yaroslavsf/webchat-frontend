53a0fa11bcc0f5294e2495b7cc998df9dbcbe1fc
BenEliasMartin <martinbenelias@gmail.com>
2024-01-04 12:04:56 +0100

    app comment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { useContext, createContext } from 'react'
+import { PreferencesProvider } from './provider/PreferencesProvider'
+
+const PreferencesContext = createContext()
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PreferencesProvider>
+      <Component {...pageProps} />
+    </PreferencesProvider>
+  );
 }
+
